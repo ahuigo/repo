cb85bf136ba4e68ca9e66663a322b3daf3a8f27e
Xuehui Ye <yexuehui@momenta.ai>
2019-08-23 23:03:38 +0800

    a

--- check.sh
+++ check.sh
@@ -1,1 +1,1 @@
-git submodule  foreach 'git status |grep -F "Your branch is up to date" ; [[ $? = 0 ]] || exit 1'
+git submodule  foreach 'git status |grep -F "Your branch is up to date" && git pull'
